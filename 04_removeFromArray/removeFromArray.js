8a41c45f472977faa524acc8cb1afc06ba14de35
Brubru <bruno.benoit98@gmail.com>
2022-11-27 19:21:53 +0100

    do javascript exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...valueToRemove) {
+    for (let value of valueToRemove){
+        indexValueToRemove = array.findIndex(element => element === value);
+        if (indexValueToRemove !== -1) {
+            array.splice([indexValueToRemove],1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    if (typeof num1 === "number" && typeof num2 === "number" && num1>=0 && num2>=0){
+        let sum=0;
+        nbOperations = Math.abs(num2-num1)+1;
+        if (num2>num1){
+            for (let i=0; i< nbOperations; i++){
+                sum+= num1+i;
+            }
+        }
+        else {
+            for (let i=0; i< nbOperations; i++){
+                sum+= num2+i;
+            }
+        }
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 
 };
 
